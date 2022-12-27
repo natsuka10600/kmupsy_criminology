707dc231a823efd138b8c5b17eed6c1f07f2fd9e
natsuka10600 <120298794+natsuka10600@users.noreply.github.com>
2022-12-27 08:57:47 +0800

    Update url.js

--- url.js
+++ url.js
@@ -19,8 +19,6 @@ const urls = [
 	'https://www.surveycake.com/s/1OLQ7',
 	'https://www.surveycake.com/s/PPlbw',
 	'https://www.surveycake.com/s/NP89o',
-	'https://www.surveycake.com/s/1OLQ7',
-	'https://www.surveycake.com/s/mZ0WM',
 	'https://www.surveycake.com/s/09ky0'
 ];
 ptt
